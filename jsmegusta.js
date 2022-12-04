dd0710a3d8813263f5df344a917c2e49a74bebfd
ChiaraAguiar <93166412+ChiaraAguiar@users.noreply.github.com>
2022-12-04 07:42:41 -0300

    Add files via upload

---
+++ jsmegusta.js
@@ -0,0 +1,18 @@
+function changevalue1(){
+    let likes = Megusta .queryselector(".increases");
+    let nn = numberparseInt(value.innerhtml)
+     nn + =; 1
+     likes.innerhtml = nn
+}
+function changevalue1(){
+    let likes = Megusta .queryselector(".increases2");
+    let nn = numberparseInt(value.innerhtml)
+     nn + =; 1
+     likes.innerhtml = nn
+}
+function changevalue2(){
+    let likes = Megusta .queryselector(".increases3");
+    let nn = numberparseInt(value.innerhtml)
+     nn + =; 1
+     likes.innerhtml = nn
+}
